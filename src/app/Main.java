b21558e869be69193742e7ce69f16e2c032dd9f5
Kadavi Savage <coedry@gmail.com>
2014-03-24 09:38:50 -0700

    init

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,6 +4,7 @@ import com.ning.http.client.AsyncHttpClient;
 import com.ning.http.client.AsyncHttpClientConfig;
 import com.ning.http.client.FilePart;
 import com.ning.http.client.Response;
+import com.sun.jna.platform.win32.*;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -89,49 +90,96 @@ public class Main extends Application {
         Runnable periodicTask = new Runnable() {
             public void run() {
 
-                String result = null;
+                WinDef.HWND currentFocusedHwnd = User32.INSTANCE.GetForegroundWindow();
 
-                try {
+                WinDef.HWND cHwnd = User32.INSTANCE.FindWindow
+                        (null, "Netflix - Google Chrome");
+                WinDef.RECT cRect = new WinDef.RECT();
 
-                    BufferedImage image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+                WinDef.HWND fHwnd = User32.INSTANCE.FindWindow
+                        (null, "Netflix - Mozilla Firefox");
+                WinDef.RECT fRect = new WinDef.RECT();
 
-                    ImageIO.write(image, "jpg", new File("C:/Users/Grant Dawson/IdeaProjects/sittingpretty/src/app/sight.jpg"));
+                if (cHwnd == null && fHwnd == null) {
 
-                } catch (Exception e) {
-                    e.printStackTrace();
+                    System.out.println("Netflix is not running");
+
                 }
+
+                List<WinDef.RECT> rois = new ArrayList<WinDef.RECT>();
+
+                if (User32.INSTANCE.IsWindowVisible(cHwnd)) {
+
+                    System.out.println("Chrome: " + User32.INSTANCE.IsWindowVisible(cHwnd));
+                    System.out.println(cRect.toString());
+                    User32.INSTANCE.GetWindowRect(cHwnd, cRect);
+                    rois.add(cRect);
+
+                }
+
+                if (User32.INSTANCE.IsWindowVisible(fHwnd)) {
+
+                    System.out.println("Firefox: " + User32.INSTANCE.IsWindowVisible(fHwnd));
+                    System.out.println(fRect.toString());
+                    User32.INSTANCE.GetWindowRect(fHwnd, fRect);
+                    rois.add(fRect);
+
+                }
+
+                String result = null;
 
                 File file = new File("C:/Users/Grant Dawson/IdeaProjects/sittingpretty/src/app/sight.jpg");
 
-                MimetypesFileTypeMap fileTypeMap = new MimetypesFileTypeMap();
+                for (int i = 0 ; i < rois.size(); i++) {
 
-                AsyncHttpClient client = new AsyncHttpClient(new AsyncHttpClientConfig.Builder().build());
-                AsyncHttpClient.BoundRequestBuilder builder = client.preparePost("http://localhost:8080/");
+                    Rectangle rect = new Rectangle();
+                    rect.setRect(rois.get(i).toRectangle().getX() + rois.get(i).toRectangle().getWidth()/2,
+                            rois.get(i).toRectangle().getY() + rois.get(i).toRectangle().getHeight()/2,
+                            rois.get(i).toRectangle().getWidth()/2,
+                            rois.get(i).toRectangle().getHeight()/2);
 
-                builder.setHeader("Content-Type", "multipart/form-data");
+                    try {
 
-                builder.addBodyPart(new FilePart("fileupload", file, fileTypeMap.getContentType(file), "UTF-8"));
+                        BufferedImage image = new Robot().createScreenCapture(rect);
 
-                try {
+                        ImageIO.write(image, "jpg", file);
 
-                    result = builder.execute().get().getResponseBody();
-
-                    if (!result.isEmpty()) {
-
-                        System.out.println(result);
-
-                        mouseClick(result.split(","));
-
-                    } else {
-
-                        System.out.println("wiff");
-
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
 
-                } catch (Exception e) {
+                    MimetypesFileTypeMap fileTypeMap = new MimetypesFileTypeMap();
 
-                    e.printStackTrace();
+                    AsyncHttpClient client = new AsyncHttpClient(new AsyncHttpClientConfig.Builder().build());
+                    AsyncHttpClient.BoundRequestBuilder builder = client.preparePost("http://localhost:8080/");
 
+                    builder.setHeader("Content-Type", "multipart/form-data");
+                    builder.addParameter("email", "coedry@gmail.com");
+                    //builder.addParameter("key", "#$@#$");
+                    builder.addBodyPart(new FilePart("fileupload", file, fileTypeMap.getContentType(file), "UTF-8"));
+
+                    try {
+
+                        result = builder.execute().get().getResponseBody();
+
+                        if (!result.isEmpty()) {
+
+                            System.out.println(result);
+
+                            if (result.contains(","))
+                                mouseClick(result.split(","), rect, currentFocusedHwnd);
+
+                        } else {
+
+                            System.out.println("\nwiff");
+
+                        }
+
+                    } catch (Exception e) {
+
+                        e.printStackTrace();
+
+                    }
                 }
             }
         };
@@ -145,47 +193,73 @@ public class Main extends Application {
         executor.shutdown();
     }
 
-    public static void mouseClick(String[] pointStrings) throws AWTException {
-        System.out.println("clicking");
+    public static void mouseClick(String[] relativePosition, Rectangle offset, WinDef.HWND refocus) throws AWTException {
+        if (relativePosition.length > 2) return;
 
         java.awt.Point current = MouseInfo.getPointerInfo().getLocation();
 
         Robot robot = new Robot();
 
-        robot.mouseMove((int)Double.parseDouble(pointStrings[0]), (int)Double.parseDouble(pointStrings[1]));
+        robot.mouseMove((int)(offset.getX() + (Double.parseDouble(relativePosition[0]))),
+                (int)(offset.getY() + Double.parseDouble(relativePosition[1])));
 
         robot.mousePress(InputEvent.BUTTON1_MASK);
         robot.mouseRelease(InputEvent.BUTTON1_MASK);
-        robot.delay(300);
+        robot.delay(200);
         robot.mousePress(InputEvent.BUTTON1_MASK);
         robot.mouseRelease(InputEvent.BUTTON1_MASK);
 
+        robot.mouseMove((int)current.getX()+1, (int)current.getY()+1);
+        robot.delay(5);
         robot.mouseMove((int)current.getX(), (int)current.getY());
+        System.out.println("Clicking: (" + ((offset.getX() + (Double.parseDouble(relativePosition[0])))) + "," + (offset.getY() + Double.parseDouble(relativePosition[1])) + ")");
+
+        User32.INSTANCE.SetForegroundWindow(refocus);
+
     }
 
-    public static void DetectCircle() {
+    /* Client-only experimentation code. */
+
+    public static void DetectCircle(String path) {
 
         List<Point> totalCircles = new ArrayList(),
                   totalTriangles = new ArrayList();
 
-        Mat src = Highgui.imread("C:/Users/Grant Dawson/IdeaProjects/sittingpretty/src/app/screenshot.jpg", CvType.CV_32F);
+        Mat src = Highgui.imread(path, CvType.CV_32FC4);
+        //src = src.submat(new Rect(320, 320, 300, 300));
+
+        //Highgui.imwrite("C:/Users/Grant Dawson/IdeaProjects/sittingpretty/src/app/cropped.jpg", src);
 
         Mat circles = new Mat();
 
-        Mat dst = Mat.zeros(src.size(), CvType.CV_32F);
+        Mat dst = Mat.zeros(src.size(), CvType.CV_32FC4);
 
-        Mat triangleResult = Mat.zeros(src.size(), CvType.CV_32F),
-              circleResult = Mat.zeros(src.size(), CvType.CV_32F);
+        Mat triangleResult = Mat.zeros(src.size(), CvType.CV_32FC4),
+              circleResult = Mat.zeros(src.size(), CvType.CV_32FC4);
 
-        Core.inRange(src, new Scalar(200,200,200), new Scalar(240,240,240), circleResult); // White circles
-        Core.inRange(src, new Scalar(0,0,175), new Scalar(10,50,250), triangleResult); // Red masses
+        Core.inRange(src, new Scalar(205,205,205), new Scalar(245,245,245), circleResult); // White circles
 
-        Imgproc.GaussianBlur(circleResult, circleResult, new Size(9, 9), 2, 2);
+        Imgproc.GaussianBlur(circleResult, circleResult, new Size(7, 7), 7);
 
-        int iCannyUpperThreshold = 10;
-        int iMinDistance = 100;
-        int iMinRadius = 20;
-        int iMaxRadius = 50;
+        Highgui.imwrite("C:/Users/Grant Dawson/IdeaProjects/sittingpretty/src/app/circleResultOne.jpg", circleResult);
+
+        Core.inRange(src, new Scalar(210,210,230), new Scalar(235,235,240), triangleResult); // White circles
+
+        Imgproc.GaussianBlur(triangleResult, triangleResult, new Size(19, 19), 19);
+
+        Core.addWeighted(circleResult, 1.0, triangleResult, 1.0, 50.0, circleResult);
+
+        Highgui.imwrite("C:/Users/Grant Dawson/IdeaProjects/sittingpretty/src/app/circleResultTwo.jpg", circleResult);
+
+        Core.inRange(src, new Scalar(15,25,150), new Scalar(65,80,240), triangleResult); // Red masses
+
+
+        Highgui.imwrite("C:/Users/Grant Dawson/IdeaProjects/sittingpretty/src/app/triangleResult.jpg", triangleResult);
+
+        int iCannyUpperThreshold = 20;
+        int iMinDistance = 20;
+        int iMinRadius = 5;
+        int iMaxRadius = 100;
         int iAccumulator = 50;
 
         Imgproc.HoughCircles(circleResult, circles, Imgproc.CV_HOUGH_GRADIENT,
@@ -203,9 +277,9 @@ public class Main extends Application {
                 Point pt = new Point(Math.round(vCircle[0]), Math.round(vCircle[1]));
                 int radius = (int)Math.round(vCircle[2]);
 
-                Core.circle(dst, pt, radius, new Scalar(255, 255, 255), 10);
+                Core.circle(dst, pt, radius, new Scalar(255, 255, 255), 1);
                 totalCircles.add(pt);
-                //System.out.println("Circle: " + pt);
+                System.out.println("Circle: " + pt);
 
             }
         }
@@ -235,7 +309,7 @@ public class Main extends Application {
                 totalTriangles.add(new Point(averageCenterX / approx.toList().size(),
                         averageCenterY / approx.toList().size()));
 
-                //System.out.println("Triangle: " + approx.toList());
+                System.out.println("Triangle: " + approx.toList());
 
             }
 
@@ -250,7 +324,9 @@ public class Main extends Application {
 
                 if (Math.abs(distanceX) < 10 && Math.abs(distanceY) < 10) {
 
-                    System.out.println(totalCircles.get(i).x + " " + totalCircles.get(i).y);
+                    System.out.println("\nMatch!\n" +
+                    "(Circle) " + totalCircles.get(i).x + " " + totalCircles.get(i).y + "\n" +
+                    "(Triangle) " + totalTriangles.get(j).x + " " + totalTriangles.get(j).y);
 
                 }
 
@@ -263,9 +339,23 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
+/*
+        BufferedImage image = null;
 
-        //DetectCircle();
+        try {
+            image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
 
+        try {
+            ImageIO.write(image, "jpg", new File("C:/Users/Grant Dawson/IdeaProjects/sittingpretty/src/app/sight.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        DetectCircle("C:/Users/Grant Dawson/IdeaProjects/sittingpretty/src/app/sight.jpg");
+*/
         launch(args);
     }
 }
