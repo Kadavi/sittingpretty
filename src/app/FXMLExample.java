ab05e4c2c1b5473d6257d2ecb84373231e3048f2
Kadavi Savage <coedry@gmail.com>
2014-03-24 17:29:43 -0700

    Adding everything.

---
+++ src/app/FXMLExample.java
@@ -0,0 +1,27 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class FXMLExample extends Application {
+    
+    public static void main(String[] args) {
+
+        Application.launch(FXMLExample.class, args);
+
+    }
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("fxml_example.fxml"));
+        
+        stage.setTitle("Polite Stare");
+        stage.setScene(new Scene(root, 300, 275));
+        stage.show();
+
+    }
+}
